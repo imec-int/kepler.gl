fca947008c672187b0cc92c5b7c2b5110de9bd0a
Bernardo J. C. Nunes <bernardo.nunes@imec.be>
2022-11-14 13:00:09 +0100

    Added tests for isGeoJsonPositionInPolygon
    - testing Polygon positions

--- test/node/utils/filter-utils-test.js
+++ test/node/utils/filter-utils-test.js
@@ -442,6 +442,47 @@ test('filterUtils -> Polygon isGeoJsonPositionInPolygon', t => {
     `${lineStringPosition.coordinates} should be in range`
   );
 
+  const polygonPosition = {
+    type: 'Polygon',
+    coordinates: [
+      [
+        [data[0][1], data[0][0]],
+        [data[0][1], data[2][0]],
+        [data[0][1], data[0][0]]
+      ]
+    ]
+  };
+
+  t.equal(
+    isGeoJsonPositionInPolygon(polygonPosition, polygonFilter.value),
+    true,
+    `${polygonPosition.coordinates} should be in range`
+  );
+
+  polygonPosition.coordinates = [[data[0].slice(0, 2), data[0].slice(2)]];
+
+  t.equal(
+    isGeoJsonPositionInPolygon(polygonPosition, polygonFilter.value),
+    false,
+    `${polygonPosition.coordinates} should not be in range`
+  );
+
+  polygonPosition.coordinates = [
+    // only the exterior Line Ring should matter (first array)
+    [
+      [data[0][1], data[0][0]],
+      [data[0][1], data[2][0]],
+      [data[0][1], data[0][0]]
+    ],
+    [data[0].slice(0, 2), data[0].slice(2)]
+  ];
+
+  t.equal(
+    isGeoJsonPositionInPolygon(polygonPosition, polygonFilter.value),
+    true,
+    `${polygonPosition.coordinates} should be in range`
+  );
+
   t.end();
 });
 
