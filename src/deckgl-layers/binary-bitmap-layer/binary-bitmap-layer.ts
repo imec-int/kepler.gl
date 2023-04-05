1613875c094a2e0fc064b24e8e22324407a9d6ec
Willem Gillis <willem.gillis@imec.be>
2023-04-05 16:09:39 +0200

    feat: added kepler layer wip

--- src/deckgl-layers/binary-bitmap-layer/binary-bitmap-layer.ts
+++ src/deckgl-layers/binary-bitmap-layer/binary-bitmap-layer.ts
@@ -1,5 +1,5 @@
-import {BitmapLayer} from '@deck.gl/layers';
-import {Buffer} from 'buffer';
+import { BitmapLayer } from '@deck.gl/layers';
+
 
 // Define a new bitmap layer type that can receive binary image data
 class BinaryBitmapLayer extends BitmapLayer {
@@ -10,31 +10,19 @@ class BinaryBitmapLayer extends BitmapLayer {
 
   initializeState() {
     super.initializeState();
-    const {gl} = this.context;
-    const {width, height} = this.props;
+    const { gl } = this.context;
 
-    // Convert binary image data to a PNG format buffer
-    const pngData = Buffer.from(this.binaryData).toString('base64');
-    const imageData = `data:image/png;base64,${pngData}`;
-
-    // Create a new image object from the PNG data
-    const image = new Image();
-    image.src = imageData;
-
-    // Once the image has loaded, create a new texture and bind it to the layer
-    image.onload = () => {
-      const texture = gl.createTexture();
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-      gl.generateMipmap(gl.TEXTURE_2D);
-      this.setState({texture});
-    };
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.R32F, gl.R32F, gl.FLOAT, this.binaryData);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    this.setState({ texture });
   }
 
   finalizeState() {
     super.finalizeState();
-    const {gl} = this.context;
-    const {texture} = this.state;
+    const { gl } = this.context;
+    const { texture } = this.state;
 
     // Cleanup the texture when the layer is removed
     gl.deleteTexture(texture);
